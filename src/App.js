2ae45c05163cbb7b4fbd1fe026817d6c3249b5c1
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-11-26 15:33:55 +0100

    Add dynamic JSX examples

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+const user = {
+  firstName: "Pablo",
+  lastName: "Perez",
+};
+
+function displayAvatar() {
+  if (user.avatarUrl) {
+    {/* no quotes when using variables for HTML attributes */}
+    return <img src={user.avatarUrl} />;
+  }
+  else {
+    return <img src="https://s3.amazonaws.com/owler-image/logo/ironhack_owler_20180828_221413_original.png" />
+  }
+}
+
+
 // a component is a CLASS that extends from React's Component class
 class App extends Component {
+  handleH2Click() {
+    alert("Hell yeah, it is!");
+  }
+
   // every component class needs a "render()" method
   // (determines the component's content)
   render() {
@@ -12,8 +32,14 @@ class App extends Component {
     return (
       // use "className" instead of "class" for styling
       <div className="App">
-        <h2>JSX is Weird</h2>
+        {/* call the "handleH2Click()" method when the H2 is clicked */}
+        <h2 onClick={() => this.handleH2Click()}>
+          JSX is Weird
+        </h2>
         <p>This is App component.</p>
+
+        <p>Welcome, {user.firstName} {user.lastName}.</p>
+        {displayAvatar()}
       </div>
     );
   }
