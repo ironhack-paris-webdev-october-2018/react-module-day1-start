1fd13e2622b83fca84d8f95c2e7c794b46d65455
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-11-26 11:04:50 +0100

    Add React Hello World

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// "import" is the new syntax that's like "require"
+// (includes external code inside this JS file)
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById("root")
+);
